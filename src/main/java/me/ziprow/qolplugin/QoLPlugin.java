3a0c82e0ee68f462a8803c301ccd6da010df92eb
Z1proW <z1prow@gmail.com>
2022-05-23 20:41:45 +0200

    implemented a config file

--- src/main/java/me/ziprow/qolplugin/QoLPlugin.java
+++ src/main/java/me/ziprow/qolplugin/QoLPlugin.java
@@ -1,22 +1,70 @@
 package me.ziprow.qolplugin;
 
+import com.google.common.io.ByteStreams;
 import me.ziprow.qolplugin.Events.*;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 
 public final class QoLPlugin extends JavaPlugin
 {
 
-	private final Listener[] events = {new Timber(this), new CloseInv(), new EditSigns(), new EditStands(), new RotateRedstone()};
-
 	@Override
 	public void onEnable()
 	{
-		regEvents();
+		loadConfig();
 		addRecipes();
 	}
 
-	private void regEvents()
+	private void loadConfig()
+	{
+		File configFile = new File(getDataFolder(), "config.yml");
+
+		if(!configFile.exists())
+		{
+			File folder = getDataFolder();
+
+			if(!folder.exists())
+				folder.mkdir();
+
+			File resourceFile = new File(folder, "config.yml");
+
+			try
+			{
+				if(!resourceFile.exists())
+				{
+					resourceFile.createNewFile();
+
+					try(InputStream in = getResource("config.yml");
+						OutputStream out = Files.newOutputStream(resourceFile.toPath()))
+					{
+						ByteStreams.copy(in, out);
+					}
+				}
+			}
+			catch(IOException e) {e.printStackTrace();}
+		}
+
+		FileConfiguration config = getConfig();
+
+		if(config.getBoolean("close-inv"))
+			regEvent(new CloseInv());
+
+		if(config.getBoolean("rotate-redstone"))
+			regEvent(new RotateRedstone());
+
+		if(config.getBoolean("timber"))
+			regEvent(new Timber(this));
+	}
+
+	private void regEvent(Listener... events)
 	{
 		for(Listener e : events)
 			getServer().getPluginManager().registerEvents(e, this);
